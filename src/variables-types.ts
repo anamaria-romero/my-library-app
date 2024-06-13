48eceb73c123b2303f58bd05b93a5e5797735f50
unknown <SenaManana@ARMAPRCCIFOD578.FormacionSena.RED>
2024-06-13 07:36:01 -0500

    setup tipado primitivo

---
+++ src/variables-types.ts
@@ -0,0 +1,29 @@
+//Declara una variable bookTitle de tipo string y asígnale el valor "TypeScript Basics".//
+var bookTitle: string = "TypeScript Basics";
+
+//Declara una constante libraryName de tipo string y asígnale el valor "My Library".//
+const libraryName: string = "My Library";
+
+//Declara una variable numberOfBooks de tipo number y asígnale un valor numérico.//
+var numberOfBooks: number = 10;
+
+//Declara una variable isLibraryOpen de tipo boolean y asígnale el valor true.//
+var isLibraryOpen: boolean = true;
+
+//Luego crea las siguientes varibles a las cuales defina que tipo de datos deben tener.//
+var titulo: string = "Pedro Páramo";
+var autor: string = "Juan Rulfo";
+var genero: string = "Ficción";
+var idioma: string = "Español";
+var precio: number = 34900;
+var formato: string = "Tapa blanda";
+var isbn: number = 9788420471976;
+var descripcion: string = "Un hombre busca a su padre en un pueblo fantasma en México.";
+var estado: string = "Como nuevo";
+var ubicacion: string = "Mercado Libre";
+var fecha_publicacion: number = 1955;
+var editorial: string = "RM Verlag";
+var paginas: number = 124;
+var dimensiones: string = "12.5 x 1 x 19.5 cm";
+var peso: string = "0.15 kg";
+var disponible: boolean = true;
